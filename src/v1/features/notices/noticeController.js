22f5b6d95dc2add037098381552bc9994fac1cf8
wematu <simplyubiquitous2020@gmail.com>
2024-10-11 06:03:08 +0000

    push notification on new notice

--- src/v1/features/notices/noticeController.js
+++ src/v1/features/notices/noticeController.js
@@ -2,9 +2,31 @@ const noticeService = require("./noticeService");
 const baseController = require("../common/baseController");
 const { asyncHandler } = require("../../middleware");
 const { SuccessResponse, ErrorResponse } = require("../../utils");
+const { Token } = require("../../../models");
+const pushQueue = require("../../queues/pushQueue");
 
 module.exports = {
     ...baseController(noticeService),
+
+    // @Description             // Create a new resource
+    // @route                   // POST api/{{resourceName}}
+    // @auth                    // Required
+    create: asyncHandler(async (req, res, next) => {
+        const data = await noticeService.create(req.body, req.user);
+
+        res.status(201).json(new SuccessResponse("Resource created", data));
+
+        const pushTokens = await Token.find({}).select("pushToken");
+
+        pushTokens.filter((t) => t.pushToken.trim() !== "").forEach(async (token) => {
+            await pushQueue.dispatch({
+                expoPushToken: token.pushToken,
+                title: "A new notice was posted",
+                body: `${data.priority}: ${data.title}`,
+                data: { type: "NewNotice", payload: { noticeId: data.id } },
+            });
+        });
+    }),
 
     // @Description             // Find all service resources
     // @route                   // GET api/{{resourceName}}
@@ -25,4 +47,5 @@ module.exports = {
 
         return res.json(new SuccessResponse("Found Resource", resource));
     }),
+
 };
